65111babfb2ef3d050e85c69a7e65a64135dca02
miamadduri <maddurimia@yahoo.com>
2019-05-03 12:25:05 -0400

    Corrected code
    Had it iterate through the string list in order to get every number in the line

--- test_reg_ex.py
+++ test_reg_ex.py
@@ -1,4 +1,5 @@
 #test extraction: using regular expressions to extract integers from a file and returning the sum
+#test extraction
 import re
 
 test_data = open('test.txt')
@@ -7,19 +8,12 @@ extracted_num = list()
 for line in test_data:
     line = line.rstrip()
     extracted = re.findall('[0-9]+', line)
-    x=0
-    if len(extracted) >0 :
-        print('String: ', extracted)
-        x += 1
-       while x>0 :
-           num = int(extracted[x])
-           extracted_num.append(num)
-print(extracted_num)
-    #num = int(extracted[0])
-    #extracted_num.append(num)
-    #extracted_num = [int(x) for x in extracted_num]
-
-
-#print(extracted_num)
-#print('Sum: ', sum(extracted_num))
+    length = len(extracted)
+    if length > 0 :
+        #print(extracted)
+        num = int(extracted[length-1])
+        extracted_num.append(num)
+    #print(extracted_num)
+     
+print('Sum of all numbers in this file: ', sum(extracted_num))
 
